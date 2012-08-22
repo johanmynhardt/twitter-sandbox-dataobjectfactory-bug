6176863e26449b9c5f8d97ae176f68e34da7dd63
Johan Mynhardt <johanmynhardt@gmail.com>
2012-08-22 12:11:44 +0200

    Initial commit: DataObjectFactory.getRawJSON tests for Weekly vs Location

---
+++ src/test/java/za/co/thumbtribe/sandbox/twitter/AppTest.java
@@ -0,0 +1,61 @@
+package za.co.thumbtribe.sandbox.twitter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import twitter4j.Trends;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.json.DataObjectFactory;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+public class AppTest {
+
+	Twitter twitter;
+
+	@Before
+	public void init() {
+		Configuration configuration = new ConfigurationBuilder().setJSONStoreEnabled(true).build();
+		twitter = new TwitterFactory(configuration).getInstance();
+	}
+
+	@Test
+	public void testDataObjectFactoryForWeeklyTrends() {
+		try {
+			List<Trends> trendsList = twitter.getWeeklyTrends();
+			System.out.println("trendsList.size() = " + trendsList.size());
+			Set<String> results = new HashSet<String>();
+			for (Trends trends : trendsList) {
+				System.out.println("Getting json for trends trending at: " + trends.getTrendAt());
+				String json = DataObjectFactory.getRawJSON(trends);
+				if (json != null) {
+					results.add(json);
+				}
+			}
+			assertFalse("Results expected!", results.isEmpty());
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testDataObjectFactoryForLocationTrends() {
+		try {
+			Trends locationTrends = twitter.getLocationTrends(1);
+			System.out.println("Getting JSON for locationTrends: " + locationTrends);
+			String json = DataObjectFactory.getRawJSON(locationTrends);
+			assertNotNull("Non-null string expected here.", json);
+			System.out.println("JSON: " + json);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+}
