692fc8cce3e7a7cf27910d6d45830718a199b698
Johan Mynhardt <johanmynhardt@gmail.com>
2012-08-22 13:07:28 +0200

    Add test for daily trends

--- src/test/java/za/co/thumbtribe/sandbox/twitter/Twitter4JDataObjectFactoryTests.java
+++ src/test/java/za/co/thumbtribe/sandbox/twitter/Twitter4JDataObjectFactoryTests.java
@@ -17,13 +17,13 @@ import twitter4j.json.DataObjectFactory;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
-public class AppTest {
+public class Twitter4JDataObjectFactoryTests {
 
 	Twitter twitter;
 
 	@Before
 	public void init() {
-		Configuration configuration = new ConfigurationBuilder().setJSONStoreEnabled(true).build();
+		Configuration configuration = new ConfigurationBuilder().setJSONStoreEnabled(true).setGZIPEnabled(true).build();
 		twitter = new TwitterFactory(configuration).getInstance();
 	}
 
@@ -34,7 +34,6 @@ public class AppTest {
 			System.out.println("trendsList.size() = " + trendsList.size());
 			Set<String> results = new HashSet<String>();
 			for (Trends trends : trendsList) {
-				System.out.println("Getting json for trends trending at: " + trends.getTrendAt());
 				String json = DataObjectFactory.getRawJSON(trends);
 				if (json != null) {
 					results.add(json);
@@ -47,13 +46,30 @@ public class AppTest {
 	}
 
 	@Test
+	public void testDataObjectFactoryForDailyTrends() {
+		try {
+			List<Trends> trendsList = twitter.getDailyTrends();
+			System.out.println("trendList.size() = " + trendsList.size());
+			Set<String> results = new HashSet<String>();
+			for (Trends trends : trendsList) {
+				String json = DataObjectFactory.getRawJSON(trends);
+				if (json != null) {
+					results.add(json);
+				}
+			}
+			assertFalse("Daily Trends expected!", results.isEmpty());
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
 	public void testDataObjectFactoryForLocationTrends() {
 		try {
 			Trends locationTrends = twitter.getLocationTrends(1);
 			System.out.println("Getting JSON for locationTrends: " + locationTrends);
 			String json = DataObjectFactory.getRawJSON(locationTrends);
 			assertNotNull("Non-null string expected here.", json);
-			System.out.println("JSON: " + json);
 		} catch (TwitterException e) {
 			e.printStackTrace();
 		}
